ff11d6b47f883f185d7767b0f5dfc7dda404d1b4
JeremyPineau <jeremailpineau@gmail.com>
2015-06-29 17:21:32 +0200

    index

--- Index.py
+++ Index.py
@@ -7,7 +7,8 @@ pygame.init() #On initialise la fenêtre
 
 #VARIABLE ET CONSTANTES
 BLANC= (255,255,255)  #Avec le modèle rgb (red, green, blue)
-
+GREY=(122,122,82)
+BROWN=(102,51,0)
 #MISE EN PLACE DE LA FENËTRE
 
 carte=pygame.display.set_mode((1600,700), RESIZABLE) # On ouvre une fenêtre graphique de 440*480 (large*hauteur) + la taille peut s'adapter
@@ -15,10 +16,9 @@ pygame.display.set_caption('PyProjet') #On nomme la fenêtre Pyprojet
 pygame.mouse.set_visible(1)
 
 carte.fill(BLANC) #On remplit la carte en blanc
-pygame.display.update() #On la met à jour
-######################################################################################################
+#pygame.display.update() #On la met à jour
+pygame.display.update()
 #On définit la structure de la carte
-
 Str_carte=[
 # 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
  [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -37,7 +37,6 @@ Str_carte=[
  [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
  [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
-
 #On met les coordonnées du tableau à l'échelle de la fenêtre
 def xReel(x):
 	x=x*80
@@ -51,27 +50,24 @@ def yReel(y):
 
 def Mur(xcoord,ycoord,genre):
 	if genre==1:
-		coulmur=BROWN
+		coul=BROWN
 	else:
-		coulchemin=GREY
-	pygame.draw.rect(carte,coulmur,(xReel(xcoord),yReel(ycoord),80,46))
-	pygame.draw.rect(carte,coulchemin,(xReel(xcoord),yReel(ycoord),80,46))
+		coul=GREY
+	pygame.draw.rect(carte,coul,(xReel(xcoord),yReel(ycoord),80,46))
 
-NbMur=0
+
+
 for x in range(0,20):
 	for y in range(0,14):
-		if Str_carte[y-1][x-1]!=0:
-			Mur(x,y,Str_carte[y-1][x-1])
-			Nbmur+=1
-		else:
-			Mur(x,y,0)
-#pygame.display.update()
-
- 
+		if Str_carte[y][x]!=0:
+			Mur(x,y,Str_carte[y][x])
+			
 
 
-#######################################################################################################
+
+#--------------------------------------------BOUCLE POUR QUE LA FENETRE REPONDE--------------------------------------------
 continuer = 1
+
 #Boucle infinie
 while continuer:
 	for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
