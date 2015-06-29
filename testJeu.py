f2f86eea7e13eaecb3b42945b2678a1318980320
ThithiFarges <farges.thibault@isep.fr>
2015-06-29 16:01:59 +0200

    Je ne sais pas

--- testJeu.py
+++ testJeu.py
@@ -7,7 +7,8 @@ pygame.init() #On initialise la fenêtre
 
 #VARIABLE ET CONSTANTES
 BLANC= (255,255,255)  #Avec le modèle rgb (red, green, blue)
-
+GREY=(122,122,82)
+BROWN=(102,51,0)
 #MISE EN PLACE DE LA FENËTRE
 
 carte=pygame.display.set_mode((1600,700), RESIZABLE) # On ouvre une fenêtre graphique de 440*480 (large*hauteur) + la taille peut s'adapter
@@ -20,9 +21,44 @@ pygame.display.update() #On la met à jour
 #On définit la structure de la carte
 Str_carte=[
 # 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
- [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0 ,0, 0 ,0, 0],
- [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0 ,0, 0 ,0, 0]
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0 ,0, 0 ,0, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+#On met les coordonnées du tableau à l'échelle de la fenêtre
+def xReel(x):
+	x=x*80
+	return x
+
+def yReel(y):
+	y=y*46
+	return y
+
+#Fonction donnant une couleur aux cases en fonction des valeurs du tableau de la carte c'est à dire Str_carte
+
+def Mur(xcoord,ycoord,col):
+	if col==1:
+		coulmur=BROWN
+	else:
+		coulchemin=GREY
+	pygame.draw.rect(Str_map,coulmur,(xReel(xcoord),yReel(ycoord),80,46))
+	pygame.draw.rect(Str_map,coulchemin,(xReel(xcoord),yReel(ycoord),80,46))
+
+
+
+#--------------------------------------------BOUCLE POUR QUE LA FENETRE REPONDE--------------------------------------------
 continuer = 1
 
 #Boucle infinie
