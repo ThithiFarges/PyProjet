a4c4718e2890b765f755bdfd45ad923cda4d1884
ThithiFarges <farges.thibault@isep.fr>
2015-07-03 14:44:16 +0200

    Début du laser !

--- ThithiTest.py
+++ ThithiTest.py
@@ -12,6 +12,7 @@ GREY=(122,122,82)
 BROWN=(102,51,0)
 BLEU=(0,0,255)
 NOIR=(0,0,0)
+YELLOW=(255, 255, 71)
 #MISE EN PLACE DE LA FENËTRE
 
 carte=pygame.display.set_mode((1500,700), RESIZABLE) # On ouvre une fenêtre graphique de 440*480 (large*hauteur) + la taille peut s'adapter
@@ -27,13 +28,13 @@ Str_carte=[
  [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
  [1,3,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2 ,2, 2 ,1, 1],
  [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+ [1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2 ,2, 2 ,1, 1],
  [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+ [1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2 ,2, 2 ,1, 1],
+ [1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2 ,2, 2 ,1, 1],
+ [1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2 ,2, 2 ,1, 1],
  [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
- [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
- [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
- [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
- [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
- [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+ [1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2 ,2, 2 ,1, 1],
  [1,2,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
  [1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1],
  [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -77,24 +78,26 @@ def Case(xcoord,ycoord,genre,tour):
 		pygame.draw.rect(carte,coul,(xReel(xcoord),yReel(ycoord),71,46))
 	else: pygame.draw.rect(carte,coul,(xcoord,ycoord,71/2,46/2))
 
-for x in range(0,20):
-	for y in range(0,14):
-		if Str_carte[y][x]!=0:
-			Case(x,y,Str_carte[y][x],1)
-			if Str_carte[y][x]==3:
-				(xperso,yperso)=Perso(x,y,1,1)
-				(xperso2,yperso2)=Perso(x,y,1,2)
-		
-pygame.display.update()
-
 def Verif(futurx,futury): #on vérifie la position du joueur
 	if Str_carte[futury/46][futurx/71]!=1:
 		return 1
 
+def egaux():
+	if xperso==xperso2 and yperso==yperso2:
+		coord=1
+	else:
+		coord=0
+	return coord
+
+
 def move(xperso,yperso,enx,eny,nb):
 	verif=Verif(xperso+enx,yperso+eny)
 	if verif==1:
-		Case(xperso,yperso,2,0)
+		coord=egaux()
+		if coord==0:
+			Case(xperso,yperso,2,0)
+		if coord==1:
+			Perso(xperso,yperso,2,3-nb)
 		xperso=xperso+enx
 		yperso=yperso+eny
 		(xperso,yperso)=Perso(xperso,yperso,0,nb)
@@ -104,12 +107,58 @@ def move(xperso,yperso,enx,eny,nb):
 		pygame.display.update()
 		return (xperso,yperso)
 
+def tirer(xperso,yperso,direction):
+	#if nb_joueur==1
+	if direction==1: #vers la gauche
+		initial=0
+		x_projectile=xReel(xperso)+xReel(initial)
+		y_projectile=40+yReel(yperso)/2
+		while x_projectile<1500:
+			for initial in range (0,3):
+				pygame.draw.rect(carte,YELLOW,(x_projectile,y_projectile,71/4,46/4)			
+				x_projectile+=71
+		return(xperso,yperso)
+	else:
+		return(xperso,yperso)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################    BOUCLE PRINCIPAL DU PROGRAMME ##################################
+
+for x in range(0,20):
+	for y in range(0,14):
+		if Str_carte[y][x]!=0:
+			Case(x,y,Str_carte[y][x],1)
+			if Str_carte[y][x]==3:
+				(xperso,yperso)=Perso(x,y,1,1)
+				(xperso2,yperso2)=Perso(x,y,1,2)
+
+pygame.display.update()
+
+tirer(2,1,1)
 
 continuer=1
 
 while continuer:
 	for event in pygame.event.get():
+
 		if event.type == KEYDOWN:
 			if event.key == K_RIGHT:
 				(xperso,yperso)=move(xperso,yperso,71,0,1)
